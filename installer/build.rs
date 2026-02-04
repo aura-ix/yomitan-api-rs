d39d7e08da1a824d69c810b80e2826addac8daef
aura-ix <173636334+aura-ix@users.noreply.github.com>
2026-02-04 16:44:10 -0500

    github actions attempt 2

--- installer/build.rs
+++ installer/build.rs
@@ -1,28 +1,40 @@
 use std::process::Command;
 use std::env;
+use std::path::{Path, PathBuf};
 
 // see https://zameermanji.com/blog/2021/6/17/embedding-a-rust-binary-in-another-rust-binary/
 fn main() {
     let profile = env::var("PROFILE").unwrap();
+
     let mut args = vec!["build", "-p", "yomitan-api-rs-client"];
     if profile == "release" {
         args.push("--release");
     }
-    
-    let alt_target_dir = format!("{}/client-target", env::var("OUT_DIR").unwrap());
-    
+
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let alt_target_dir = out_dir.join("client-target");
+
     let status = Command::new("cargo")
         .args(&args)
         .env("CARGO_TARGET_DIR", &alt_target_dir)
-        .current_dir("..")
+        .current_dir(Path::new(".."))
         .status()
         .expect("Failed to build client");
-    
+
     if !status.success() {
         panic!("Client build failed");
     }
-    
-    let client_path = format!("{}/{}/yomitan-api-rs-client", alt_target_dir, profile);
-    println!("cargo:rustc-env=CLIENT_BINARY_PATH={}", client_path);
+
+    let mut _client_path = alt_target_dir.join(&profile).join("yomitan-api-rs-client");
+
+    #[cfg(windows)]
+    {
+        _client_path.set_extension("exe");
+    }
+
+    println!(
+        "cargo:rustc-env=CLIENT_BINARY_PATH={}",
+        _client_path.display()
+    );
     println!("cargo:rerun-if-changed=../client/src");
 }

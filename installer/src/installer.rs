db4923e5d08819fa8f08f03803386c000869b489
aura-ix <173636334+aura-ix@users.noreply.github.com>
2026-02-04 22:02:20 -0500

    fix prompt

--- installer/src/installer.rs
+++ installer/src/installer.rs
@@ -57,7 +57,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 #[cfg(target_os = "windows")]
                 println!("You should make sure that your browser is fully closed, and wait a minute or so before trying to install again. Otherwise the files that need to be modified may be in use by the browser.");
 
-                if prompt("Try again? (enter to quit, 'y' to try again)")?.len() == 0 {
+                if prompt("Try again? (enter to quit, 'y' to try again): ")?.len() == 0 {
                     break Err(err)
                 }
             }
